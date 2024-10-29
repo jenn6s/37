97b66873e14323926739db2aeb3b1b70a13d3d62
Student <Student@DAEDMAC46.localdomain>
2024-10-29 16:30:32 -0400

    Submitting python

---
+++ python_1/3,7.py
@@ -0,0 +1,12 @@
+
+def equations(vars):
+    x, y = vars
+    eq1 = x + y - 10  # Example equation 1
+    eq2 = x*2 + y*2 - 25  # Example equation 2
+    return [eq1, eq2]
+
+# Initial guess
+initial_guess = (1, 1)
+
+solution = (equations, initial_guess)
+print("Solution:", solution)
